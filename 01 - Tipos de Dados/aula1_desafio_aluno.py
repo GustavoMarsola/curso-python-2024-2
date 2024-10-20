bb8f5a96037dc5f23bbe7e939c4087e92a413eb5
Gustavo-Marsola <gustavo.marsola@solfacil.com.br>
2024-10-20 08:18:29 -0300

    add exercicios aula 1

--- 01 - Tipos de Dados/aula1_desafio_aluno.py
+++ 01 - Tipos de Dados/aula1_desafio_aluno.py
@@ -1,4 +1,4 @@
-# Descrição: Criar um sistema que calcula o custo da estadia de um hóspede no hotel.
+# Exercício 1: Calculo do custo da estadia de um hóspede no hotel.
 
 # Objetivo: O aluno deve usar variáveis, tipos numéricos e booleanos.
 
@@ -6,3 +6,18 @@
 
 # O aluno deve perguntar ao usuário quantos dias ele ficará (número).
 # Valor da diária é de R$ 127,00 (número) .
+
+
+
+# Exercício 2: Verifique se os dados do cliente estão corretos em relação ao seu registro no banco de dados
+
+# Dados preenchidos pelo cliente
+
+nome = "Joao da silva souza"
+idade = '27'
+incluso_cafe_da_manha = 0
+
+# Dados do banco de dados
+nome_db = 'JOAO DA SILVA SOUZA'
+idade_db = 27
+incluso_cafe_da_manha_db = False
