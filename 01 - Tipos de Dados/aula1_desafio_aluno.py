cb8ccf1cab87235ac5b2960f666fcb552051af68
lucastracosa <129684161+lucastracosa@users.noreply.github.com>
2024-11-07 08:07:55 -0300

    Update aula1_desafio_aluno.py

--- 01 - Tipos de Dados/aula1_desafio_aluno.py
+++ 01 - Tipos de Dados/aula1_desafio_aluno.py
@@ -4,6 +4,19 @@
 
 # Requisitos:
 
+usuario = 'Lucas Oliveira'
+cantidad_diaria = int('3')
+custo_diaria = 'R$ 127,00'
+custo_diaria = custo_diaria.replace('R$ ', '').replace(',','.')
+custo_diaria = float(custo_diaria)
+
+
+custo_estadia = cantidad_diaria*custo_diaria
+custo_estadia = str(custo_estadia).replace('.',',')
+print('custo de la estadia estadia: R$', custo_estadia)
+
+
+
 # O aluno deve perguntar ao usuário quantos dias ele ficará (número).
 # Valor da diária é de R$ 127,00 (número) .
 
@@ -21,3 +34,18 @@ incluso_cafe_da_manha = 0
 nome_db = 'JOAO DA SILVA SOUZA'
 cpf_db = 12345678900
 incluso_cafe_da_manha_db = False
+
+
+
+conferir_nome = nome.upper()==nome_db.upper()
+# o mais adequado para este caso seria a segunda solução comentada linha 43
+cpf_db = str(cpf_db).replace('90','9-0')
+#cpf_db = cpf_db.replace('-', '')
+#cpf_db = int(cpf_db)
+
+# para selecionar comand c (copiar) + Comand D (selecionar varias recorrencias) + comand v (colar)
+conferir_cpf = cpf==cpf_db
+conferir_cafe = bool(incluso_cafe_da_manha)==incluso_cafe_da_manha_db
+print(conferir_nome)
+print(conferir_cpf)
+print(conferir_cafe)
