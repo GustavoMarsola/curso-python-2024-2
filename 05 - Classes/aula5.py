406b20bc573466df23dd6f26ad72505a9ec2ecc5
Gustavo-Marsola <gustavo.marsola@solfacil.com.br>
2024-11-19 17:50:00 -0300

    aula5 classes

---
+++ 05 - Classes/aula5.py
@@ -0,0 +1,12 @@
+class SistemaHotel:
+    def __init__(self, email: str, senha: str):
+        self.email = email
+        self.senha = senha
+        
+        self.token = None
+    
+    # Autenticar usuário
+    
+    # Registrar um hospede
+    
+    # Remover um hospede
